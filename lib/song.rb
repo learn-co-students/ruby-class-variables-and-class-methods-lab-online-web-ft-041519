1e6a3d2749543d034efef4cd24edf724e3ddd5f5
Runa V <runavalora@gmail.com>
2019-05-05 06:30:26 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -22,18 +22,6 @@ class Song
     @@artists.uniq
   end
 
-  def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1
-      else
-        artist_count[artist] = 1
-      end
-    end
-    artist_count
-  end
-
   def self.genres
     @@genres.uniq
   end
@@ -48,5 +36,17 @@ class Song
       end
     end
     genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
   end
 end
